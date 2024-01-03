7ad614bf882988f977d4d38999331e7b63f40c2d
Naki <naki1114@naver.com>
2024-01-03 17:20:53 +0000

    Chore : 이메일, 닉네임 중복 확인 후 회원 정보 저장

--- File/UserInfo/FinishSignup.php
+++ File/UserInfo/FinishSignup.php
@@ -4,6 +4,37 @@
 
   if ($connect) {
     // 연결 성공
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $id = $_POST['id'];
+      $password = $_POST['password'];
+      $nickname = $_POST['nickname'];
+
+      if ($id) {
+        $duplicateCheckQuery = "SELECT * FROM userInfo WHERE id='$id'";
+        $result = mysqli_query($connect, $duplicateCheckQuery);
+  
+        if (mysqli_num_rows($result) > 0) {  
+          echo "unusableID";
+        }
+        else {
+          if ($nickname) {
+            $duplicateCheckQuery = "SELECT * FROM userInfo WHERE nickname='$nickname'";
+            $result = mysqli_query($connect, $duplicateCheckQuery);
+      
+            if (mysqli_num_rows($result) > 0) {  
+              echo "unusableNickname";
+            }
+            else {
+              $saveUserInfoQuery = "INSERT INTO userInfo(id, password, nickname, checkingFishCount, checkingFishTicket, removeAdTicket, type) VALUES('$id', '$password', '$nickname', '3', '0', '0', 'FM');";
+              $result = mysqli_query($connect, $saveUserInfoQuery);
+              echo "usable";
+            }
+            mysqli_close($connect);
+          }
+        }
+        mysqli_close($connect);
+      }
+    }
   }
   else {
     // 연결 실패
