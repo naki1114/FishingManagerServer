5e92cf3d7df7044fb784a16beabbe9a7b6a2fde0
Naki <naki1114@naver.com>
2024-01-30 15:27:15 +0000

    Chore : 게시글 저장

--- File/Feed/InsertFeed.php
+++ File/Feed/InsertFeed.php
@@ -4,6 +4,18 @@
 
   if ($connect) {
     // 연결 성공
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $nickname = $_POST['nickname'];
+      $title = $_POST['title'];
+      $content = $_POST['content'];
+      $date = $_POST['date'];
+
+      $insertFeedQuery = "INSERT INTO feed(nickname, title, content, viewCount, date) VALUES('$nickname', '$title', '$content', 0, $date);";
+      $resultFeed = mysqli_query($connect, $insertFeedQuery);
+      $arrayFeed = array();
+
+      echo $nickname;
+    }
   }
   else {
     // 연결 실패
