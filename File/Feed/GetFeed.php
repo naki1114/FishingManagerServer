affe8f35468dca24ffc82d650a417ec94cf2cdb4
Naki <naki1114@naver.com>
2024-01-30 15:24:51 +0000

    Chore : Feed Data 클라이언트로 송부

--- File/Feed/GetFeed.php
+++ File/Feed/GetFeed.php
@@ -4,6 +4,26 @@
 
   if ($connect) {
     // 연결 성공
+    // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      
+      // ArrayList<Feed>
+      $getFeedQuery = "SELECT nickname, num, title, content, picture, viewCount, date FROM feed;";
+      $resultFeed = mysqli_query($connect, $getFeedQuery);
+      $arrayFeed = array();
+
+      while ($row = mysqli_fetch_array($resultFeed)) {
+        array_push($arrayFeed, array("nickname" => $row[0],
+                                     "feedNum" => $row[1],
+                                     "title" => $row[2],
+                                     "content" => $row[3],
+                                     "feedImage" => $row[4],
+                                     "viewCount" => $row[5],
+                                     "date" => $row[6]));
+      }
+
+      echo json_encode($arrayFeed, JSON_UNESCAPED_UNICODE);
+
+    // }
   }
   else {
     // 연결 실패
