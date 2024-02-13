34cdd85023308169e5a62718cfe07429630ddc35
Naki <naki1114@naver.com>
2024-02-13 08:03:27 +0000

    Chore : History, Feed 배열 역순으로 전송

--- File/Splash/GetUserData.php
+++ File/Splash/GetUserData.php
@@ -4,6 +4,66 @@
 
   if ($connect) {
     // 연결 성공
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $nickname = $_POST['nickname'];
+
+      // UserInfo
+      $getUserInfoQuery = "SELECT checkingFishCount, checkingFishTicket, removeAdTicket FROM userInfo WHERE nickname='$nickname';";
+      $resultUserInfo = mysqli_query($connect, $getUserInfoQuery);
+      $row = mysqli_fetch_array($resultUserInfo);
+      $userInfo = array("checkingFishCount" => $row[0],
+                        "checkingFishTicket" => $row[1],
+                        "removeAdTicket" => $row[2]);
+
+      // ArrayList<Collection>
+      $getCollectionQuery = "SELECT * FROM collection;";
+      $resultCollection = mysqli_query($connect, $getCollectionQuery);
+      $arrayCollection = array();
+
+      while ($row = mysqli_fetch_array($resultCollection)) {
+        array_push($arrayCollection, array("nickname" => $row[0],
+                                           "fishName" => $row[1],
+                                           "fishLength" => $row[2],
+                                           "date" => $row[3]));
+      }
+
+      // ArrayList<History>
+      $getHistoryQuery = "SELECT * FROM history;";
+      $resultHistory = mysqli_query($connect, $getHistoryQuery);
+      $arrayHistory = array();
+
+      while ($row = mysqli_fetch_array($resultHistory)) {
+        array_push($arrayHistory, array("nickname" => $row[0],
+                                        "fishName" => $row[1],
+                                        "fishImage" => $row[2],
+                                        "fishLength" => $row[3],
+                                        "date" => $row[4]));
+      }
+
+      $arrayHistory = array_reverse($arrayHistory);
+
+      // ArrayList<Feed>
+      $getFeedQuery = "SELECT nickname, num, title, content, picture, viewCount, date FROM feed;";
+      $resultFeed = mysqli_query($connect, $getFeedQuery);
+      $arrayFeed = array();
+
+      while ($row = mysqli_fetch_array($resultFeed)) {
+        array_push($arrayFeed, array("nickname" => $row[0],
+                                     "feedNum" => $row[1],
+                                     "title" => $row[2],
+                                     "content" => $row[3],
+                                     "feedImage" => $row[4],
+                                     "viewCount" => $row[5],
+                                     "date" => $row[6]));
+      }
+
+      $arrayFeed = array_reverse($arrayFeed);
+
+      echo json_encode(array("userInfo" => $userInfo,
+                             "collection" => $arrayCollection,
+                             "history" => $arrayHistory,
+                             "feed" => $arrayFeed), JSON_UNESCAPED_UNICODE);
+    }
   }
   else {
     // 연결 실패
