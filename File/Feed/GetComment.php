653da20132f0864eabddd579fd42f80c8a678743
Naki <naki1114@naver.com>
2024-02-21 14:45:56 +0000

    Chore : 댓글 불러오기 기능 추가4

--- File/Feed/GetComment.php
+++ File/Feed/GetComment.php
@@ -4,6 +4,24 @@
 
   if ($connect) {
     // 연결 성공
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $num = $_POST['feedNum'];
+      
+      // ArrayList<Comment>
+      $getCommentQuery = "SELECT num, nickname, content, date FROM comment WHERE num = '$num';";
+      $resultComment = mysqli_query($connect, $getCommentQuery);
+      $arrayComment = array();
+
+      while ($row = mysqli_fetch_array($resultComment)) {
+        array_push($arrayComment, array("feedNum" => $row[0],
+                                     "nickname" => $row[1],
+                                     "content" => $row[2],
+                                     "date" => $row[3]));
+      }
+
+      echo json_encode($arrayComment, JSON_UNESCAPED_UNICODE);
+
+    }
   }
   else {
     // 연결 실패
