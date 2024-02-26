567ce8a6f36d9500aa426ef5c6b153f863210ee2
Naki <naki1114@naver.com>
2024-02-26 07:12:10 +0000

    Chore : 게시글 저장
    - 이미지 파일 유무 확인하여 저장

--- File/Feed/InsertFeed.php
+++ File/Feed/InsertFeed.php
@@ -10,11 +10,44 @@
       $content = $_POST['content'];
       $date = $_POST['date'];
 
-      $insertFeedQuery = "INSERT INTO feed(nickname, title, content, viewCount, date) VALUES('$nickname', '$title', '$content', 0, $date);";
-      $resultFeed = mysqli_query($connect, $insertFeedQuery);
+      if ($_FILES) {
+
+        $uploadDir = '../../Image/FeedImage/';
+        $uploadedFile = $uploadDir.basename($_FILES['uploadFile']['name']);
+        move_uploaded_file($_FILES['uploadFile']['tmp_name'], $uploadedFile);
+  
+        $picture = "Image/FeedImage/".$_FILES['uploadFile']['name'];
+  
+        $insertFeedQuery = "INSERT INTO feed(nickname, title, content, picture, viewCount, date) VALUES('$nickname', '$title', '$content', '$picture', 0, '$date');";
+        $resultFeed = mysqli_query($connect, $insertFeedQuery);
+  
+      }
+      else {
+        
+        $insertFeedQuery = "INSERT INTO feed(nickname, title, content, viewCount, date) VALUES('$nickname', '$title', '$content', 0, '$date');";
+        $resultFeed = mysqli_query($connect, $insertFeedQuery);
+  
+      }
+      
+      // ArrayList<Feed>
+      $getFeedQuery = "SELECT nickname, num, title, content, picture, viewCount, date FROM feed;";
+      $resultFeed = mysqli_query($connect, $getFeedQuery);
       $arrayFeed = array();
 
-      echo $nickname;
+      while ($row = mysqli_fetch_array($resultFeed)) {
+        array_push($arrayFeed, array("nickname" => $row[0],
+                                     "feedNum" => $row[1],
+                                     "title" => $row[2],
+                                     "content" => $row[3],
+                                     "feedImage" => $row[4],
+                                     "viewCount" => $row[5],
+                                     "date" => $row[6]));
+      }
+
+      $arrayFeed = array_reverse($arrayFeed);
+
+      echo json_encode($arrayFeed, JSON_UNESCAPED_UNICODE);
+
     }
   }
   else {
