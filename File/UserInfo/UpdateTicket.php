5a14c580a7822243b82a7bb878ccf7f3ccaaa9d6
Naki <naki1114@naver.com>
2024-03-19 06:08:09 +0000

    Chore : 어종 확인 이용권 외 이용권들 제거

--- File/UserInfo/UpdateTicket.php
+++ File/UserInfo/UpdateTicket.php
@@ -5,42 +5,30 @@
   if ($connect) {
     // 연결 성공
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      $nickname = ['nickname'];
-      $product = ['product'];
+      $nickname = $_POST['nickname'];
+      $product = $_POST['product'];
 
-      if ($product == "FM 세트 한 달 이용권") {
-        $ticketQuery = "UPDATE userInfo SET checkingFishTicket = 30, removeAdTicket = 30 WHERE nickname = '$nickname';";
-        $result = mysqli_query($connect, $ticketQuery);
-        echo "success";
-      }
-      elseif ($product == "FM 세트 일 년 이용권") {
-        $ticketQuery = "UPDATE userInfo SET checkingFishTicket = 365, removeAdTicket = 365 WHERE nickname = '$nickname';";
-        $result = mysqli_query($connect, $ticketQuery);
-        echo "success";
-      }
-      elseif ($product == "어종 확인 한 달 이용권") {
+      if ($product == "어종 확인 한 달 이용권") {
         $ticketQuery = "UPDATE userInfo SET checkingFishTicket = 30 WHERE nickname = '$nickname';";
         $result = mysqli_query($connect, $ticketQuery);
-        echo "success";
       }
       elseif ($product == "어종 확인 일 년 이용권") {
         $ticketQuery = "UPDATE userInfo SET checkingFishTicket = 365 WHERE nickname = '$nickname';";
         $result = mysqli_query($connect, $ticketQuery);
-        echo "success";
       }
-      elseif ($product == "광고 제거 한 달 이용권") {
-        $ticketQuery = "UPDATE userInfo SET removeAdTicket = 30 WHERE nickname = '$nickname';";
-        $result = mysqli_query($connect, $ticketQuery);
-        echo "success";
-      }
-      elseif ($product == "광고 제거 일 년 이용권") {
-        $ticketQuery = "UPDATE userInfo SET removeAdTicket = 365 WHERE nickname = '$nickname';";
-        $result = mysqli_query($connect, $ticketQuery);
-        echo "success";
-      }
-      else {
-        echo "fail";
-      }
+      
+      // UserInfo
+      $getUserInfoQuery = "SELECT nickname, profileImage, checkingFishCount, checkingFishTicket, removeAdTicket, type FROM userInfo WHERE nickname='$nickname';";
+      $resultUserInfo = mysqli_query($connect, $getUserInfoQuery);
+      $row = mysqli_fetch_array($resultUserInfo);
+      $userInfo = array("nickname" => $row[0],
+                        "profileImage" => $row[1],
+                        "checkingFishCount" => $row[2],
+                        "checkingFishTicket" => $row[3],
+                        "removeAdTicket" => $row[4],
+                        "type" => $row[5]);
+
+      echo json_encode($userInfo, JSON_UNESCAPED_UNICODE);
 
     }
   }
@@ -52,4 +40,5 @@
     print "$errno : $error\n";
     exit();
   }
+
 ?>
